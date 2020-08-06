bc997d2d7a525e530ebf6365341800f5a725e786
hidekazu <basawaworks@gmail.com>
2020-08-06 14:14:00 +0900

    yahoo_search.py

--- yahoo_search.py
+++ yahoo_search.py
@@ -4,7 +4,7 @@
 # In[ ]:
 
 
-#Yahoo!検索ワード自動入力＆画面キャプチャ
+#Yhoo!検索ワード自動入力
 from selenium import webdriver
 import time
 
@@ -20,12 +20,4 @@ serch_word = input("検索キーワードを入力してください。")
 search_box.send_keys(serch_word)
 search_btn = driver.find_element_by_class_name('PHOgFibMkQJ6zcDBLbga8')
 search_btn.click()
-
-time.sleep(5)#5秒停止
-
-w = driver.execute_script('return document.body.scrollWidth')
-h = driver.execute_script('return document.body.scrollHeight')
-driver.set_window_size(w, h)
-driver.save_screenshot('screenshot.png')#キャプチャ画像出力
-driver.quit()#ブラウザを閉じる
 
